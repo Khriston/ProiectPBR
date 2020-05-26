28eea66754fa980bd4f1aad64a8ca88845dfa1c7
Khriston <33064635+Khriston@users.noreply.github.com>
2020-05-26 19:09:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pylog
 import re
+import fileinput
 
 _INTEX = 0
 _MS = "Mutare simpla"
@@ -8,11 +9,12 @@ _MI = "Mutare invalida"
 
 nr=r"((\d\d)|(\d))" #pozitii de la 1 la 24
 tranzitie=f"({nr}\/{nr}(\*)?)" #tranzitie intre doiua pozitii cu mancat sau nu
+mutare_0=f"(\d\d:)"
 mutare_1=f"(\d\d: {tranzitie})" #muta doar o piesa (zaruri normale dar cu o singura mutare posibila)
 mutare_2=f"(\d\d: {tranzitie} {tranzitie})" #poate muta doua piese (mutare normala)
 mutare_3=f"(\d\d: {tranzitie} {tranzitie} {tranzitie})" #muta trei piese (zaruri duble dar cu trei mutari posibile)
 mutare_4=f"(\d\d: {tranzitie} {tranzitie} {tranzitie} {tranzitie})" #muta patru piese(dubla normala)
-Mutari=f"({mutare_4}|{mutare_3}|{mutare_2}|{mutare_1})"
+Mutari=f"({mutare_4}|{mutare_3}|{mutare_2}|{mutare_1})"#|{mutare_0})"
 
 defs = {
 	Mutari: _MS
@@ -34,8 +36,11 @@ def extrage_mutari(text):
 			return res
 
 text = ''
+player = 0
 f = open(f"input.txt", "r", encoding='utf8')
+o = open(f"output.txt", "w", encoding='utf8')
 text = f.read()
+f.close()
 text1 = re.split('(( ){2,})', text)
 #for item in text1:
 #	print(item)
@@ -43,8 +48,49 @@ text1 = re.split('(( ){2,})', text)
 for expr in text1:
 	t = extrage_mutari(expr)
 	if len(t) != 0:
-		if len(t) == 2:
-			print(f'move({t[0]})')
-			print(f'move({t[1]})')
+		replace1 = re.sub('\d\d:( )?', '', t[0])
+		replace2 = re.sub(' ' ," ,"+str(player+1)+'\n', replace1)
+		replace3 = re.sub('\* ', ",-", replace2)
+		replace4 = re.sub(' ,', ',+,', replace3)
+		replace5 = re.sub('/', ',', replace4)
+		if(replace5[-1] == "*"):
+			replace6 = replace5.replace('*', '')
+			final = f'{replace6},-,'
 		else:
-			print(f'move({t[0]})')
+			final = f'{replace5},+,'
+		o.write(f'{final}')
+		o.write(str(player+1))
+		o.write('\n')
+		player = (player + 1) % 2
+		if len(t) == 2:
+			replace1 = re.sub('(\d\d:( )?)', '', t[1])
+			replace2 = re.sub(' ', " ," + str(player + 1) + '\n', replace1)
+			replace3 = re.sub('\* ', ",-", replace2)
+			replace4 = re.sub(' ,', ',+,', replace3)
+			replace5 = re.sub('/', ',', replace4)
+			if (replace5[-1] == "*"):
+				replace6 = replace5.replace('*', '')
+				final = f'{replace6},-,'
+			else:
+				final = f'{replace5},+,'
+			o.write(f'{final}')
+			o.write(str(player+1))
+			o.write('\n')
+			player = (player + 1) % 2
+o.close()
+o = open(f"output.txt", "r", encoding='utf8')
+text=o.read()
+o.close()
+f = open(f"output.txt", "w", encoding='utf8')
+f.truncate(0)
+text1=text.split('\n')
+for line in text1:
+	elem = line.split(',')
+	if(elem[-1] == "1"):
+		elem[0] = str(25 - int(elem[0]))
+		elem[1] = str(25 - int(elem[1]))
+	sep = ","
+	fin = sep.join(elem)
+	f.write(fin+'\n')
+f.close()
+
